fc8343a9bb92d9237cc46e2593925e69ed457361
Adam Holt <ads.noob@gmail.com>
2011-09-29 10:43:51 +0100

    http bundler deploy hook test

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,12 @@
-f = File.open("/home/deploy/deploy-config.json","w+") do |f|
-  f.write "User: #{user}"
-  f.write "Application: #{app}"
-  f.write @configuration.to_json
+include EY::Serverside::LoggedOutput
+
+Dir.chdir(release_path) do
+  require "rubygems"
+  require "bundler/setup"
+
+  require "httparty"
+
+  # You can also use post, put, delete, head, options in the same fashion
+  response = HTTParty.get('http://twitter.com/statuses/public_timeline.json')
+  [response.body, response.code, response.message, response.headers.inspect].each { |m| info(m) }
 end
