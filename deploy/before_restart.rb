fa2f5e1e3aab22d3a7109307949736c06e06baf3
Adam Holt <ads.noob@gmail.com>
2011-10-02 08:55:52 +0100

    log output from httparty call

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,15 +1,15 @@
 Dir.chdir(release_path) do
   require "rubygems"
-  require "bundler/setup"
-  # Bundler.setup(:deploy)
+  require "bundler"
+  Bundler.setup(:deploy)
 
   require "httparty"
 
   # You can also use post, put, delete, head, options in the same fashion
   response = HTTParty.get('http://twitter.com/statuses/public_timeline.json')
-  # Do something with response
+
+  File.open("/home/deploy/deploy-config.json", "w") do |f|
+    f.write response.inspect
+  end
 end
 
-File.open("/home/deploy/deploy-config.json", "w") do |f|
-  f.write("Deploying User: #{deploy_user}") if deploy_user
-end
