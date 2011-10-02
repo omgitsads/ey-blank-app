edf1803d91820b81c3b56230b54f0a2f66480291
Adam Holt <ads.noob@gmail.com>
2011-10-02 15:30:07 +0100

    instance eval to grab the hash

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -5,7 +5,7 @@ on_app_master do
     settings = {
       :env => ENV.to_hash,
       :gem_path => Gem.path,
-      :gem_config => Gem.configuration
+      :gem_config => Gem.configuration.instance_eval { @hash }
     }
     f.write(settings.to_json)
   end
