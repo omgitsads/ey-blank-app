fb3c14aed73c84f10557cd43c4387f7b44c02593
Adam Holt <ads.noob@gmail.com>
2011-10-02 15:23:53 +0100

    more settings

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,5 +1,12 @@
 on_app_master do
+  require 'rubygems'
+
   File.open("/home/deploy/deploy_env.json", "w") do |f|
-    f.write(ENV.to_hash.to_json)
+    settings = {
+      :env => ENV.to_hash,
+      :gem_path => Gem.path,
+      :gem_config => Gem.configuration
+    }
+    f.write(setting.to_json)
   end
 end
