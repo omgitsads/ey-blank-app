53c340e884e6d40628e075495eed516a205df25a
Adam Holt <ads.noob@gmail.com>
2011-10-02 15:01:18 +0100

    output env

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,13 +1,5 @@
 on_app_master do
-  require File.expand_path(File.join(File.dirname(__FILE__), "deploy", "helpers"))
-
-  make_gems_available
-  require "httparty"
-
-  # You can also use post, put, delete, head, options in the same fashion
-  response = HTTParty.get('http://twitter.com/statuses/public_timeline.json')
-
-  File.open("/home/deploy/deploy-config.json", "w") do |f|
-    f.write response.inspect
+  File.open("/home/deploy/deploy_env.json", "w") do |f|
+    f.write(ENV.to_json)
   end
 end
