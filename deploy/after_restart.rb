9e41e78bd13722dd2941b17243f562d21493cb31
Adam Holt <me@adamholt.co.uk>
2012-02-14 10:13:17 +0000

    output the repo in the log

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,12 +1,2 @@
-on_app_master do
-  require 'rubygems'
-
-  File.open("/home/deploy/deploy_env.json", "w") do |f|
-    settings = {
-      :env => ENV.to_hash,
-      :gem_path => Gem.path,
-      :gem_config => Gem.configuration.instance_eval { @hash }
-    }
-    f.write(settings.to_json)
-  end
-end
+info "Repo: #{repo}"
+debug "Repo: #{repo}"
