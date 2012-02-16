7fdc3b34d6e3181883250a9b7f852f626e85d787
Adam Holt <me@adamholt.co.uk>
2012-02-16 17:04:02 +0000

    tweaks from live

--- lib/middleware_start.rb
+++ lib/middleware_start.rb
@@ -8,15 +8,17 @@ module NewRelic
       begin
         now = (Time.now.to_f * 1000000).to_i
         env["HTTP_X_MIDDLEWARE_START"] = "t=#{now}"
-    
+
         # Calculate the queue time
-        if env["HTTP_X_SERVER_START"]
-          server_time = env["HTTP_X_SERVER_START"][2,16].to_i
+        if env["HTTP_X_REQUEST_START"]
+          server_time = env["HTTP_X_REQUEST_START"][2,16].to_i
           queue_time = now - server_time
 
+          env['rack.errors'].write "Server Time: #{server_time} - Queue Time: #{queue_time}"
           env["HTTP_X_QUEUE_TIME"] = "t=#{queue_time}"
         end
       ensure
+        env['rack.errors'].write "Trying to add Headers!"
         return @app.call(env)
       end
     end
