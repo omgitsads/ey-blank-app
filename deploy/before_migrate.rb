7bb7dadfc069dd507fc17be80fca5e8d2f87fd2b
Adam Holt <me@adamholt.co.uk>
2012-04-26 15:18:39 +0100

    env stuff

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,2 +1,5 @@
 run "ruby -e \"require 'pp'; pp ENV.inspect\" > /home/deploy/env-before-migrate.txt"
-puts ENV['RAILS_ENV']
+
+File.open("/home/deploy/rails_env.txt", "w") do |f|
+  f.write "RAILS_ENV: #{ENV['RAILS_ENV']}"
+end
