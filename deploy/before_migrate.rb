cca7064ffae4522bd6a50a01c9d68ed3297aa2ec
Adam Holt <me@adamholt.co.uk>
2012-05-25 14:27:56 +0100

    remove the raise

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,5 +3,3 @@ run "ruby -e \"require 'pp'; pp ENV.inspect\" > /home/deploy/env-before-migrate.
 File.open("/home/deploy/rails_env.txt", "w") do |f|
   f.write "RAILS_ENV: #{ENV['RAILS_ENV']}"
 end
-
-raise RuntimeError, "With a message"
